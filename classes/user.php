32aaab299f56246e27f89610ea10ef26c7728e96
Alan luu <luumanhquan.91@gmail.com>
2019-12-18 21:32:35 +0700

    lop user

---
+++ classes/user.php
@@ -0,0 +1,39 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/LapTrinhWeb/lib/database.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/LapTrinhWeb/helper/format.php';
+?>
+<?php
+  class User
+  {
+    private $db;
+    private $fm;
+    function __construct(argument)
+    {
+      $this->db = new Database();
+      $this->fm = new Format();
+    }
+    $user=new
+    public insert_User($Name,$pass){
+      $Name=$this->fm->valation($Name);
+      $Pass=$this->fm->valation($Pass);
+
+      $Name=mysqli_real_escape_string($this->db->link,$Name);
+      $Pass=mysqli_real_escape_string($this->db->link,$Pass);
+
+      if(empty($Name) || empty($Pass)){
+        $alert="user must be not empty";
+        return $alert;
+      }else{
+        $sql="INSERT INTO user(UserName, Password, RoleID) VALUES($Name,$Pass,'user')";
+        $result=$this->db->insert($sql);
+        if($result){
+          $alert="<span>insert user successen </span>"
+          return $alert;
+        }else{
+          $alert="<span>insert no successen</span>"
+        }
+      }
+    }
+  }
+
+?>
