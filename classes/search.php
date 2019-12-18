68d8c52ecc796d0489a2d3311fba57fa128ac466
Alan luu <luumanhquan.91@gmail.com>
2019-12-18 13:28:13 +0700

    search

--- classes/search.php
+++ classes/search.php
@@ -2,6 +2,7 @@
 $filePath = realpath(dirname(__FILE__));
 include_once($filePath . '/../lib/database.php');
 include_once($filePath . '/../helper/format.php');
+include_once 'product.php'
  ?>
 <?php
   /**
@@ -9,9 +10,25 @@ include_once($filePath . '/../helper/format.php');
    */
   class search
   {
-    function __construct(argument)
+    public $product=new product();
+    private $db;
+    private $fm;
+
+    public function __construct(argument)
     {
-      // code...
+      $this->db = new Database();
+      $this->fm = new Format();
+    }
+
+    public function searchProduct($Name){
+      if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        $productName=$_POST['productName'];
+      }
+      $pos=strpos($productName,$Name);
+
+      if($pos===false){
+        echo "<span>no found prodouct name </span>";
+      }
     }
   }
 
