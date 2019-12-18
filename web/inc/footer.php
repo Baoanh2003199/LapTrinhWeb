6e7ec70436351c0615f0c9279badc8072cd1b196
Alan luu <luumanhquan.91@gmail.com>
2019-12-18 21:33:27 +0700

    dang nhap va dang xuat

--- web/inc/footer.php
+++ web/inc/footer.php
@@ -1,50 +1,106 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/LapTrinhWeb/classes/login.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/LapTrinhWeb/classes/customer.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/LapTrinhWeb/classes/user.php';
+include_once $_SERVER['DOCUMENT_ROOT']."/QQA-LapTrinhWeb/lib/session.php";
+Session::checkSession();
+?>
+<?php
+$class = new login();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $User = $_POST['User'];
+  $Pass = md5($_POST['Pass']);
+
+  $login_check = $class->login($User, $Pass);
+}
+?>
+<?php
+  $register=new user();
+  $cuss=new customer();
+  if($_SERVER['REQUEST_METHOD']==='POST'){
+      $Name=$_POST['Name'];
+      $Pass=$_POST['Pass'];
+      $Email=$_POST['Email'];
+      $Phone=$_POST['Phone'];
+      $Address=$_POST['Address'];
+      $City=$_POST['city'];
+      $DoB=$_POST['DoB'];
+      $Status=$_POST['null'];
+
+      $registerUser=$register->insert_User($Name,$Pass);
+      $registerCuss=$cuss->insert_category($Name,$Address,$Phone,$Email,$City,$DoB,$Status);
+  }
+?>
 <!-- Footer -->
 <div id="acountManager">
   <ul>
     <li id="btnLogin"> Đăng nhập</li>
     <li id="btnRegister"> Đăng ký</li>
-    <li id=""> Đăng xuất</li>
+    <?php
+          if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+            Session::destroy();
+          }
+    ?>
+    <li id="?action=logout"> Đăng xuất</li>
   </ul>
 </div>
 <div id="divLogin">
   <span>Đăng nhập</span>
-  <form>
+  <?php
+  if(isset($login_check)){
+    echo $login_check;
+  }
+  ?>
+  <form method="POST">
     <div class="form-group">
       <label for="exampleInputEmail1">Tên tài khoản</label>
-      <input type="text" class="form-control" id="txtUsernamelg" aria-describedby="emailHelp"
-        placeholder="Tên đăng nhập">
+      <input type="text" class="form-control" id="txtUsernamelg" aria-describedby="emailHelp" name="User" placeholder="Tên đăng nhập">
       <small id="emailHelp" class="form-text text-muted"></small>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Mật khẩu</label>
-      <input type="password" class="form-control" id="txtPasswordlg" placeholder="Mật khẩu">
+      <input type="password" class="form-control" id="txtPasswordlg" placeholder="Mật khẩu" name="Pass">
     </div>
     <button type="submit" class="btn btn-primary">Đăng nhập</button>
     <button type="button" class="btn btn-light" id="btnDel">Hủy</button>
   </form>
 </div>
-<div id="divRegister">
+<div id="divRegister" method="post">
   <span>Đăng ký</span>
+  <?php
+      if($registerUser && $registerCuss){
+        echo $registerUser;
+        echo $registerCuss;
+      }
+   ?>
   <form>
     <div class="form-group">
       <label for="txtUserName">Tên tài khoản</label>
-      <input type="text" class="form-control" id="txtUserName" aria-describedby="emailHelp" placeholder="Tên đăng nhập">
+      <input type="text" class="form-control" id="txtUserName" aria-describedby="emailHelp" placeholder="Tên đăng nhập" name="Name">
     </div>
     <div class="form-group">
       <label for="texPassword">Mật khẩu</label>
-      <input type="password" class="form-control" id="texPassword" placeholder="Mật khẩu">
+      <input type="password" class="form-control" id="texPassword" placeholder="Mật khẩu" name="Pass">
     </div>
     <div class="form-group">
       <label for="txtEmail">Email</label>
-      <input type="email" class="form-control" id="txtEmail" placeholder="Email">
+      <input type="email" class="form-control" id="txtEmail" placeholder="Email" name="Email">
     </div>
     <div class="form-group">
       <label for="txtPhone">Số điện thoại</label>
-      <input type="text" class="form-control" id="txtPhone" placeholder="Số điện thoại">
+      <input type="text" class="form-control" id="txtPhone" placeholder="Số điện thoại" name="Phone">
     </div>
     <div class="form-group">
       <label for="txtPhone">Địa chỉ</label>
-      <input type="text" class="form-control" id="txtAddress" placeholder="Địa chỉ">
+      <input type="text" class="form-control" id="txtAddress" placeholder="Địa chỉ" name="Address">
+    </div>
+    <div class="form-group">
+      <label for="txtPhone">city</label>
+      <input type="text" class="form-control" id="txtAddress" placeholder="city" name="city">
+    </div>
+    <div class="form-group">
+      <label for="txtPhone">DoB</label>
+      <input type="text" class="form-control" id="txtAddress" placeholder="DoB" name="DoB">
     </div>
     <button type="submit" class="btn btn-primary">Đăng ký</button>
     <button type="button" class="btn btn-light" id="btnDelR">Hủy</button>
