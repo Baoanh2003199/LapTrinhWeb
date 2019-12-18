5557b172911c78ee59abae97bf57ad15c265f088
unknown <minhquangle71@gmail.com>
2019-12-18 13:36:58 +0700

    Update xong front-end có gì ko hiểu nhắn hỏi tui

--- templates/resource/web/myscript.js
+++ templates/resource/web/myscript.js
@@ -1,3 +1,43 @@
-function name(params) {
-    
+function setDisplay(divName) {
+    $(divName).css("display", 'block');
+   
+}
+function closeDisplay(divName) {
+    $(divName).css("display", 'none');
+}
+function setReadyDocument(){
+    $('#btnOrder').click(function (e) { 
+        setDisplay('#paymentDiv');
+        
+    });
+    $('#closePayment').click(function (e) { 
+        closeDisplay('#paymentDiv');
+        
+    });
+    $('#btnAcount').hover(function () {
+        setDisplay('#acountManager');
+            
+        }, function () {
+            closeDisplay('#acountManager');
+        }
+    );
+    $('#acountManager').hover(function () {
+        setDisplay('#acountManager');
+            
+        }, function () {
+            closeDisplay('#acountManager');
+        }
+    );
+    $('#btnLogin').click(function (e) { 
+        setDisplay('#divLogin');
+    });
+    $('#btnDel').click(function (e) { 
+        closeDisplay('#divLogin');
+    });
+    $('#btnRegister').click(function (e) { 
+        setDisplay('#divRegister');
+    });
+    $('#btnDelR').click(function (e) { 
+        closeDisplay('#divRegister');
+    });
 }
