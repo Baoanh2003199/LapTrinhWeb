50ae89af9978f729bca53cfa893d5d77ca578146
unknown <minhquangle71@gmail.com>
2019-12-18 11:21:00 +0700

    update

--- database/shopOnline.sql
+++ database/shopOnline.sql
@@ -1,129 +1,82 @@
 
-drop database if exists shoponline;
 create database shoponline;
-use shoponline;
 
-create table Suppliers
-(
-	SupplierID int not null
-	AUTO_INCREMENT,
-	SupplierName nvarchar
-	(50),
-	Address varchar
-	(50),
-	Phone varchar
-	(13),
+create table Suppliers(
+	SupplierID int not null AUTO_INCREMENT,
+	SupplierName nvarchar(50),
+	Address varchar(50),
+	Phone varchar(13),
 	Status int,
-	Primary key
-	(SupplierID)
+	Primary key(SupplierID) 
 );
-	create table Categories
-	(
-		CategoryID int not null
-		AUTO_INCREMENT,
-	CategoryName varchar
-		(50),
-	Description varchar
-		(255),
+create table Categories(
+	CategoryID int not null AUTO_INCREMENT,
+	CategoryName varchar(50),
+	Description varchar(255),
     Status int,
-	primary key
-		(CategoryID)
+	primary key(CategoryID)
 );
-		create table Products
-		(
-			ProductID int not null
-			AUTO_INCREMENT,
-	ProductName varchar
-			(50),
+create table Products(
+	ProductID int not null AUTO_INCREMENT,
+	ProductName varchar(50),
 	Price float,
 	Views int,
     SellNumber int,
-    Origin varchar
-			(50),
-	Img varchar
-			(100),
-	Description varchar
-			(255),
+    Origin varchar(50),
+	Img varchar(100),
+	Description varchar(255),
 	CategoryID int,
 	SupplierID int,
 	Status int,
 
 
-	primary key
-			(ProductID)
+	primary key(ProductID)
 );
-			create table Roles
-			(
-				RoleID int not null
-				AUTO_INCREMENT,
-	RoleName varchar
-				(50),
-	RoleCode varchar
-				(20),
+create table Roles(
+	RoleID int not null AUTO_INCREMENT,
+	RoleName varchar(50),
+	RoleCode varchar(20),
 
-	primary key
-				(RoleID)
+	primary key(RoleID)
 );
-				create table User
-				(
-					UserID int not null
-					AUTO_INCREMENT,
-	UserName varchar
-					(20),
-	Password varchar
-					(50),
+create table User(
+	UserID int not null AUTO_INCREMENT,
+	UserName varchar(20),
+	Password varchar(50),
 	RoleId int,
     Status int,
-
-    primary key
-					(UserID)
+    
+    primary key(UserID)
 );
-					create table Employees
-					(
-						EmployeeID int not null
-						AUTO_INCREMENT,
-	Name varchar
-						(50),
-	Address varchar
-						(50),
-	Phone varchar
-						(13),
-	Email varchar
-						(30),
+create table Employees(
+	EmployeeID int not null AUTO_INCREMENT,
+	Name varchar(50),
+	Address varchar(50),
+	Phone varchar(13),
+	Email varchar(30),
     DoB datetime,
 	RoleID int,
 	UserId int,
     Status int,
-
-	Primary key
-						(EmployeeID)
+	
+	Primary key(EmployeeID)
 );
-						create table Customers
-						(
-							CustomerID int not null
-							AUTO_INCREMENT,
-	Name varchar
-							(50),
-	Address varchar
-							(50),
-	Phone varchar
-							(13),
-	Email varchar
-							(30),
-    City varchar
-							(50),
+create table Customers(
+	CustomerID int not null AUTO_INCREMENT,
+	Name varchar(50),
+	Address varchar(50),
+	Phone varchar(13),
+	Email varchar(30),
+    City varchar(50),
     DoB datetime,
 	RoleID int,
 	UserId int,
     Status int,
-
-	Primary key
-							(CustomerID)
+    
+	Primary key(CustomerID)
 );
-							create table Cart
-							(
-								CartID int not null
-								auto_increment,
+create table Cart(
+	CartID int not null auto_increment,
 	ProductID int,
     CustomerID int,
 	Quantity int,
@@ -131,29 +84,24 @@ create table Suppliers
     Status int,
     CreatedDate datetime,
 
-	Primary key
-								(CartID)
+	Primary key(CartID)
 );
-								create table Orders
-								(
-									OrderID int not null
-									auto_increment,
+create table Orders(
+	OrderID int not null auto_increment,
 	Total float,
     QuantityProducts int,
-	Note varchar
-									(255),
+	Note varchar(255),
+    Address varchar(50),
     Status int,
-
-    primary key
-									(OrderID)
-
+    
+    primary key(OrderID)
+	
 );
-									create table OrderDetails
-									(
-										CartID int,
-										OrderID int,
-										Note varchar(255),
-										Status int,
-
-										primary key(CartID, OrderID)
-									);
+create table OrderDetails(
+	CartID int,
+    OrderID int,
+    Note varchar(255),
+    Status int,
+    
+    primary key(CartID, OrderID)
+);
