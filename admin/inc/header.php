6e96e5162a77863db65e52d0ccdea883739acf5e
ANH HỒ BẢO <1760259@student.hcmus.edu.vn>
2019-12-27 07:19:29 +0700

    Commit change

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -9,7 +9,7 @@ ob_start();
 
 <head>
   <meta charset="UTF-8" />
-  <link href="../templates/images/logo.jpg" rel="icon" type="image/x-icon">
+  <link href="../templates/images/icon.png" rel="icon" type="image/x-icon">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <link rel="stylesheet" href="../templates/vendor/css/bootstrap.min.css" />
